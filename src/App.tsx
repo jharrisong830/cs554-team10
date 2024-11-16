11d2b8d6cc9b416cb546d42fa3cf9cf68901dac5
John Graham <110198683+jharrisong830@users.noreply.github.com>
2024-11-15 19:35:46 -0500

    functions done, need to write logic for sending requests

--- src/App.tsx
+++ src/App.tsx
@@ -32,6 +32,9 @@ function App() {
             <p className="read-the-docs">
                 Click on the Vite and React logos to learn more
             </p>
+            <div className="card">
+                <a href="/auth">Log in to spotify</a>
+            </div>
         </>
     );
 }
---
+++ src/lib/spotify/data.ts
@@ -0,0 +1,128 @@
+/**
+ * data functions for spotify api
+ */
+
+import { Track, Album } from "./types";
+
+const SPOTIFY_CLIENT_ID = "afb9fc797fd2485abe86d74540b42c77";
+const pkcePossible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-.~";
+
+/**
+ * builds the URL for the initial spotify authorization
+ *
+ * redirects to spotify login, which returns authorization code on success
+ *
+ * @param codeChallenge code challenge to be sent as a query param
+ * @returns authorization endpoint used to get user authorization code
+ */
+export const getAuthorizationURL = (codeChallenge: string): string => {
+    const scope = "playlist-read-private user-read-private user-library-read"; // TODO: refine scopes to be minimal
+    const userAuthQuery = {
+        response_type: "code",
+        redirect_uri: "http://localhost:3000",
+        client_id: import.meta.env.VITE_SPOTIFY_CLIENT_SECRET,
+        scope: scope,
+        code_challenge_method: "S256",
+        code_challenge: codeChallenge
+    };
+
+    const authURL =
+        "https://accounts.spotify.com/authorize?" + // construct http query
+        Object.entries(userAuthQuery)
+            .map(([k, v]) => `${k}=${v}`)
+            .join("&");
+
+    return authURL;
+};
+
+/**
+ * generates a cryptographically secure string of given length in [43, 128] according to the PKCE protocol
+ *
+ * reference for implementation: https://developer.spotify.com/documentation/web-api/tutorials/code-pkce-flow
+ *
+ * @param {number} length   length of the randomly generated string
+ *
+ * @returns {string} randomly generated string according to PKCE standard
+ * @throws when length is invalid
+ */
+const generatePKCEString = (length = 64): string => {
+    if (typeof length !== "number")
+        throw `invalid length, expected type number, received ${typeof length} (${length})`;
+    if (length < 43 || length > 128 || length % 1 !== 0)
+        throw `invalid length, expected integer in [43, 128], received ${length}`;
+
+    const randVals = crypto.getRandomValues(new Uint8Array(length)); // fill a byte array of size length with random values
+    return randVals.reduce(
+        (acc, curr) => acc + pkcePossible[curr % pkcePossible.length],
+        ""
+    ); // get each character in the string
+};
+
+/**
+ * encrypt str with sha256 algorithm
+ *
+ * reference for implementation: https://developer.spotify.com/documentation/web-api/tutorials/code-pkce-flow
+ *
+ * @param str message to be encrypted
+ * @returns sha256 encrypted message buffer
+ */
+const encrypt = async (str: string): Promise<ArrayBuffer> => {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(str);
+    return await crypto.subtle.digest("SHA-256", data); // encrypt with sha256 algo
+};
+
+/**
+ * base-64 encode a byte buffer
+ *
+ * reference for implementation: https://developer.spotify.com/documentation/web-api/tutorials/code-pkce-flow
+ *
+ * @param input buffer to be encoded
+ * @returns base-64 encoded string
+ */
+const base64encode = (input: ArrayBuffer): string => {
+    return btoa(String.fromCharCode(...new Uint8Array(input)))
+        .replace(/=/g, "")
+        .replace(/\+/g, "-")
+        .replace(/\//g, "_");
+};
+
+/**
+ * returns a triplet of PKCE codes to use for authorization
+ *
+ * @param length length of the randomly generated string
+ * @returns object containing codeVerifier, hashed, and codeChallenge
+ * @throws when length is invalid
+ */
+export const getPKCECodes = async (length: number = 64) => {
+    const codeVerifier = generatePKCEString(length); // will throw with incorrect length
+    const hashed = await encrypt(codeVerifier);
+    const codeChallenge = base64encode(hashed);
+
+    return {
+        codeVerifier: codeVerifier,
+        hashed: hashed,
+        codeChallenge: codeChallenge
+    };
+};
+
+// const getTrack = async (accessToken: string, trackId: string): Track => {
+//     const data = await fetch(`https://api.spotify.com/v1/tracks/${trackId}`, {
+//         method: "GET",
+//         headers: {
+//             Authorization: `Bearer ${accessToken}`
+//         }
+//     });
+
+//     return {
+//         _id: data.id, // all info is stored within a "track" subobject in the items array
+//         platform: "SP", // hardcoded bc duh...
+//         type: "track", // again...
+//         isrc: data.external_ids.isrc,
+//         name: data.name,
+//         artists: data.artists.map((a) => a.name), // extract only the names
+//         platformURL: data.external_urls.spotify,
+//         albumId: data.album.id
+//     };
+// };
