f6cb87ea6df138d9e744335049057c206fdd4762
John Graham <110198683+jharrisong830@users.noreply.github.com>
2024-12-13 13:55:50 -0500

    sharp fix, added synthetic imports flag to TS

--- api/process-image.ts
+++ api/process-image.ts
@@ -1,5 +1,5 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
-import * as sharp from "sharp";
+import sharp from "sharp";
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method === "POST") {
     try {
